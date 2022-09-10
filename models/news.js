f8cbb3003da2fdf9051d4afa537c06f62261959a
aaron <kaven22314567@gmail.com>
2022-09-10 15:20:42 +0800

    [feat] update news (order)

--- models/news.js
+++ models/news.js
@@ -1,7 +1,7 @@
 const pool = require('../utils/db');
 
 async function getAllNews() {
-  let [data] = await pool.query(`SELECT news.*, news_category.name AS categoryName FROM news JOIN news_category ON news.category = news_category.id `);
+  let [data] = await pool.query(`SELECT news.*, news_category.name AS categoryName FROM news JOIN news_category ON news.category = news_category.id ORDER BY id DESC `);
   return data;
 }
 
