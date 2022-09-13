90d0889ec902fb6add4b9d0a797dd4e727a16e3a
Sandy <a0919613087@gmail.com>
2022-09-13 19:56:20 +0800

    [feat] add auth

---
+++ middlewares/auth.js
@@ -0,0 +1,11 @@
+let checkLogin = function (req, res, next) {
+  // 判斷這個人是否已經登入？
+  // session 裡如果沒有 member 這個資料，表示沒有登入過
+  if (!req.session.user) {
+    //尚未登入
+    return res.status(403).json({ msg: '尚未登入' });
+  }
+  next();
+};
+
+module.exports = { checkLogin };
