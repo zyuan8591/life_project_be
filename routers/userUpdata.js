1eaf9aab2c36601a5fa03a6662e12689e8ce9317
Quan <cat814051@gmail.com>
2022-09-15 13:23:48 +0800

    [add]UPDATE password

--- routers/userUpdata.js
+++ routers/userUpdata.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const pool = require('../utils/db');
 const path = require('path');
 const multer = require('multer');
+const argon2 = require('argon2');
+const { body, validationResult } = require('express-validator');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -49,5 +51,34 @@ router.put('/', uploader.single('photo'), async (req, res) => {
   let [users] = await pool.execute('SELECT * FROM users WHERE id= ?', [req.session.user.id]);
   let user = users[0];
   res.json(user);
+});
+module.exports = router;
+
+//密碼修改
+const reqistetRules = [
+  body('password').isLength({ min: 8 }).withMessage('請輸入8個字以上的半形英文數字'),
+  body('newPassword').isLength({ min: 8 }).withMessage('請輸入8個字以上的半形英文數字'),
+  body('confirmPassword')
+    .custom((value, { req }) => {
+      return value === req.body.newPassword;
+    })
+    .withMessage('「密碼」不一致'),
+];
+router.put('/password', reqistetRules, async (req, res) => {
+  const validateResult = validationResult(req);
+  if (!validateResult.isEmpty()) {
+    return res.status(400).json({ errors: validateResult.array() });
+  }
+  let [users] = await pool.execute('SELECT * FROM users WHERE id= ?', [req.session.user.id]);
+  let user = users[0];
+  //驗證舊密碼有無相符合
+  let verifyResult = await argon2.verify(user.password, req.body.password);
+  if (!verifyResult) {
+    return res.status(401).json({ message: '舊密碼錯誤' });
+  }
+  let hashPassword = await argon2.hash(req.body.newPassword, 10);
+
+  await pool.execute('UPDATE users SET password=? WHERE id=?', [hashPassword, req.session.user.id]);
+  res.json('密碼修改成功');
 });
 module.exports = router;
